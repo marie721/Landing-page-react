f624bf2872fab52f511746e66bec4e0ccd41fdfc
sarahmachicado <colorsthemind@gmail.com>
2020-06-20 16:19:53 +0000

    added home.js changes

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -1,6 +1,63 @@
 import React from "react";
 
 function Navbar() {
-	return <h2>{"hello navbar"}</h2>;
+	return (
+		<div className="container-fluid">
+			<nav className="navbar navbar-expand-lg navbar-dark bg-primary d-flex flex-row justify-content-between">
+				<div className="container">
+					<a className="navbar-brand" href="#">
+						Kitty Store
+					</a>
+					<ul className="navbar-nav ">
+						<li className="nav-item active">
+							<a className="nav-link" href="#">
+								Home <span className="sr-only">(current)</span>
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								About
+							</a>
+						</li>
+						<li className="nav-item dropdown">
+							<a
+								className="nav-link dropdown-toggle"
+								href="#"
+								id="navbarDropdown"
+								role="button"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false">
+								My kitty
+							</a>
+							<div
+								className="dropdown-menu"
+								aria-labelledby="navbarDropdown">
+								<a className="dropdown-item" href="#">
+									Kitties Available
+								</a>
+								<a className="dropdown-item" href="#">
+									Breed Directory
+								</a>
+								<div className="dropdown-divider" />
+								<a className="dropdown-item" href="#">
+									Search for Cats
+								</a>
+							</div>
+						</li>
+						<li className="nav-item">
+							<a
+								className="nav-link"
+								href="#"
+								tabIndex="-1"
+								aria-disabled="true">
+								Contact
+							</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	);
 }
 export default Navbar;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Jumbotron from "./jumbotron";
+import Carddeck from "./carddeck.js";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="container">
+		<div className="nav-top">
 			<Navbar />
-			<Jumbotron />
+			<div className="container">
+				<Jumbotron />
+				<Carddeck />
+			</div>
 		</div>
 	);
 }
